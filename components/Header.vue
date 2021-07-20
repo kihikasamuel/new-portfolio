6df52e9f82a0e96e5813b4221b09b2e9531b2727
Summitm <summitros@gmail.com>
2021-07-20 17:06:37 +0300

    light-mode

--- components/Header.vue
+++ components/Header.vue
@@ -38,3 +38,24 @@
         </nav>
     </header>
 </template>
+
+<script>
+export default {
+    mounted() {
+        let $switch = document.querySelector('.bi-moon');
+        $switch.addEventListener('click', (event)=>{
+            event.preventDefault();
+
+            let body = document.querySelector("body");
+            body.style.backgroundColor = "#FFFFFF";
+
+            body.style.opacity = "1";
+            for(let prop of document.querySelectorAll("p")) {
+                prop.style.color = "#000000";
+            }
+            
+            document.querySelector(".bi-moon").style.color = "#000000";
+        });
+    }
+}
+</script>
